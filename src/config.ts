26bf8c75046e89456c63451fc908acbf015e4908
Dennis Snell <dennis.snell@automattic.com>
2018-04-19 02:45:33 +0100

    actually add config file

---
+++ src/config.ts
@@ -0,0 +1,48 @@
+import * as Dockerode from 'dockerode';
+import * as bunyan from 'bunyan';
+import * as portfinder from 'portfinder';
+import { LoggerOptions, DEBUG } from 'bunyan';
+
+interface AppConfig {
+    build: BuildConfig;
+
+    repo: RepoConfig;
+
+    server: ServerConfig;
+};
+
+interface BuildConfig {
+    containerCreateOptions: Dockerode.ContainerCreateOptions;
+    logFilename: string;
+    logLevel: bunyan.LogLevel;
+    tagPrefix: string;
+};
+
+interface RepoConfig {
+    project: string;
+};
+
+interface ServerConfig {
+    logLevel: bunyan.LogLevel;
+};
+
+export const config: AppConfig = {
+    build: {
+        containerCreateOptions: {
+            Env: ['NODE_ENV=wpcalypso', 'CALYPSO_ENV=wpcalypso'],
+            ExposedPorts: { '3000/tcp': {} },
+            Tty: false,
+        },
+        logFilename: 'dserve-build-log.txt',
+        logLevel: 'debug',
+        tagPrefix: 'dserve-wpcalypso'
+    },
+
+    repo: {
+        project: 'Automattic/wp-calypso'
+    },
+
+    server: {
+        logLevel: 'debug',
+    },
+};
