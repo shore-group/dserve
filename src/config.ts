41eef2159a42978f6a64165d074a75e8c0b0e13d
Dennis Snell <dennis.snell@automattic.com>
2018-04-20 13:56:57 +0100

    interface > type; add ;s;

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
 import * as Dockerode from 'dockerode';
 
-type primitive = string | number | boolean | undefined | null
-type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>
+type primitive = string | number | boolean | undefined | null;
+type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>;
 type DeepReadonlyObject<T> = {
   readonly [P in keyof T]: DeepReadonly<T[P]>
-}
+};
 
-interface AppConfig {
+type AppConfig = {
     build: BuildConfig;
     repo: RepoConfig;
 };
 
-interface BuildConfig {
+type BuildConfig = {
     containerCreateOptions: Dockerode.ContainerCreateOptions;
     logFilename: string;
     tagPrefix: string;
 };
 
-interface RepoConfig {
+type RepoConfig = {
     project: string;
 };
 
